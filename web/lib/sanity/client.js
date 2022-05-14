a6dbe8a7cd1ef8b09d6327f65b2a7815d1eea57f
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2022-05-14 12:01:59 -0400

    testing sanity branch

--- web/lib/sanity/client.js
+++ web/lib/sanity/client.js
@@ -1,7 +1,7 @@
 const sanityClient = require("@sanity/client");
 export const client = sanityClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_ID,
-  dataset: "production",
+  dataset: "expose-noir",
   // set to production when time for launch
   apiVersion: '2022-04-27',
   //   token: 'sanity-auth-token', // or leave commented out to be anonymous user
