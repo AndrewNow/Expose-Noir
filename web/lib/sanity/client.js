4380d73ce4a478a22dec17c0482d5d331ee05122
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2022-05-15 13:07:43 -0400

    testing dataset

--- web/lib/sanity/client.js
+++ web/lib/sanity/client.js
@@ -2,7 +2,6 @@ const sanityClient = require("@sanity/client");
 export const client = sanityClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_ID,
   dataset: "expose-noir",
-  // set to production when time for launch
   apiVersion: '2022-04-27',
   //   token: 'sanity-auth-token', // or leave commented out to be anonymous user
   useCdn: true, // `false` if you want to ensure fresh data
