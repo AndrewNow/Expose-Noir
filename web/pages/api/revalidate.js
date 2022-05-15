079da911d99bce7f1120f6705a98acc0c84cec01
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2022-05-15 16:07:19 -0400

    moving function to top

--- web/pages/api/revalidate.js
+++ web/pages/api/revalidate.js
@@ -1,29 +1,3 @@
-export async function handler(request, response) {
-  const Authorization = request.headers.authorization || "";
-  const token = Authorization.replace(/bearer/i, "").trim();
-
-  if (request.method !== "POST") {
-    return response.status(405).send("Method Not Allowed");
-  }
-
-  if (token !== process.env.NEXT_SANITY_WEBHOOK_SECRET) {
-    return response.status(401).send("Invalid token");
-  }
-
-  try {
-    const path = getPathToRevalidate(request.body);
-
-    if (path) {
-      await response.unstable_revalidate(path);
-    }
-
-    return response.status(204).send("No Content");
-  } catch (error) {
-    console.error(error);
-    return response.status(500).send("Internal Server Error");
-  }
-}
-
 const getPathToRevalidate = (body) => {
   switch (body._type) {
     case "eventDescription": {
@@ -48,6 +22,31 @@ const getPathToRevalidate = (body) => {
       return null;
   }
 
+  export async function handler(request, response) {
+    const Authorization = request.headers.authorization || "";
+    const token = Authorization.replace(/bearer/i, "").trim();
+
+    if (request.method !== "POST") {
+      return response.status(405).send("Method Not Allowed");
+    }
+
+    if (token !== process.env.NEXT_SANITY_WEBHOOK_SECRET) {
+      return response.status(401).send("Invalid token");
+    }
+
+    try {
+      const path = getPathToRevalidate(request.body);
+
+      if (path) {
+        await response.unstable_revalidate(path);
+      }
+
+      return response.status(204).send("No Content");
+    } catch (error) {
+      console.error(error);
+      return response.status(500).send("Internal Server Error");
+    }
+  }
 
   // switch (body._type) {
   //   case "post": {
