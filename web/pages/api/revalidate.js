ff8ca3c470985864e27023f891075fce49fe62a4
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2022-05-15 16:10:35 -0400

    moving function hotfix

--- web/pages/api/revalidate.js
+++ web/pages/api/revalidate.js
@@ -21,40 +21,39 @@ const getPathToRevalidate = (body) => {
     default:
       return null;
   }
-}
+};
 
-  export async function handler(request, response) {
-    const Authorization = request.headers.authorization || "";
-    const token = Authorization.replace(/bearer/i, "").trim();
+export async function handler(request, response) {
+  const Authorization = request.headers.authorization || "";
+  const token = Authorization.replace(/bearer/i, "").trim();
 
-    if (request.method !== "POST") {
-      return response.status(405).send("Method Not Allowed");
-    }
-
-    if (token !== process.env.NEXT_SANITY_WEBHOOK_SECRET) {
-      return response.status(401).send("Invalid token");
-    }
-
-    try {
-      const path = getPathToRevalidate(request.body);
-
-      if (path) {
-        await response.unstable_revalidate(path);
-      }
-
-      return response.status(204).send("No Content");
-    } catch (error) {
-      console.error(error);
-      return response.status(500).send("Internal Server Error");
-    }
+  if (request.method !== "POST") {
+    return response.status(405).send("Method Not Allowed");
   }
 
-  // switch (body._type) {
-  //   case "post": {
-  //     return `/` + body.slug.current;
-  //   }
-  //   // Your other cases for other types here
-  //   default:
-  //     return null;
-  // }
-};
+  if (token !== process.env.NEXT_SANITY_WEBHOOK_SECRET) {
+    return response.status(401).send("Invalid token");
+  }
+
+  try {
+    const path = getPathToRevalidate(request.body);
+
+    if (path) {
+      await response.unstable_revalidate(path);
+    }
+
+    return response.status(204).send("No Content");
+  } catch (error) {
+    console.error(error);
+    return response.status(500).send("Internal Server Error");
+  }
+}
+
+// switch (body._type) {
+//   case "post": {
+//     return `/` + body.slug.current;
+//   }
+//   // Your other cases for other types here
+//   default:
+//     return null;
+// }
