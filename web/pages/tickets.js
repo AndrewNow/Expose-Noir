0afd67f40debed037addb374ff7a5149000bfb96
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2023-08-29 12:56:46 -0400

    changing /past to /archive

--- web/pages/tickets.js
+++ web/pages/tickets.js
@@ -218,7 +218,7 @@ const Tickets = ({
                   </DownloadPoster>
                 )}
                 <br />
-                <Link href="/past">past bookings</Link>
+                <Link href="/archive">archive</Link>
                 <br />
                 <a href="mailto:complaints@exposenoir.com">
                   complaints@exposenoir.com
